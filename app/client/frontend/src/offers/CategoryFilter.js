7ea3035a7d7ac86cedb4e6ff361e6d911256a559
Esaiasson <teo.esaiasson@gmail.com>
2024-08-28 09:18:48 +0200

    remove old code

--- app/client/frontend/src/offers/CategoryFilter.js
+++ app/client/frontend/src/offers/CategoryFilter.js
@@ -8,13 +8,6 @@ const CategoryFilter = (props) => {
     const [categories, setCategories] = useState([])
     const [nbrOfLevels, setNbrOfLevels] = useState(1)
 
-/*
-    const categories = [
-        {'id': 'aeb778ee-939d-44e4-8f57-8810c3347dae', 'name': 'Lokaler'},
-        {'id': '6a319924-f2d3-4813-8103-7ad7fc7676ce', 'name': 'Evenemang'},
-        {'id': '2b6af113-55d2-4bcc-9066-d17196ffd745', 'name': 'Övrigt'}
-    ];
-*/
     useEffect(() => {
         getCategories();
     }, []);
