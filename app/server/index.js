487ab99e7f9243e61db0908de85c091f61199052
Esaiasson <teo.esaiasson@gmail.com>
2024-09-17 13:25:45 +0200

    added the server back

--- app/server/index.js
+++ app/server/index.js
@@ -1,25 +1,27 @@
-import express from 'express';
+import { createServer } from './server.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { config } from 'dotenv';
 
+// Load environment variables from .env
 config();
 
-const app = express();
 const port = process.env.PORT || 5432;
+const server = createServer();
 
 // __filename and __dirname are not available in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/frontend/build')));
+server.use(express.static(path.join(__dirname, '../client/frontend/build')));
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
+// Handle any non-API routes by serving the React app
+server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/frontend/build', 'index.html'));
 });
 
-app.listen(port, () => {
+// Start the server
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
