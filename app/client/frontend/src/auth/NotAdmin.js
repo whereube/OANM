9713b0d453bfb53be772fae0fea2bee8b344becc
Esaiasson <teo.esaiasson@gmail.com>
2024-09-17 09:31:32 +0200

    added button styles to the navigate options

--- app/client/frontend/src/auth/NotAdmin.js
+++ app/client/frontend/src/auth/NotAdmin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import './NotAdmin.css'
 
 
 const NotAdmin = () => { 
@@ -11,11 +12,11 @@ const NotAdmin = () => {
     } 
 
     return (
-        <>
+        <div className='notAdminPage'>
             <h2>Du behöver vara inloggad som en administratör för att komma åt denna sida</h2>
-            <p onClick={() => handleNavigate('/article/showAll')}>Klicka här för att komma tillbaka till startsidan</p>
-            <p onClick={() => handleNavigate("/profile/login")}>Klicka här för att logga in</p>
-        </>
+            <div className='button-small notAdminButton' onClick={() => handleNavigate('/article/showAll')}>Klicka här för att komma tillbaka till startsidan</div>
+            <div className='button-small notAdminButton' onClick={() => handleNavigate("/profile/login")}>Klicka här för att logga in</div>
+        </div>
     );
 };
 
