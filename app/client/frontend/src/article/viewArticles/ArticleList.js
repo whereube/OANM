5b9e6496611f40d1f5914211e21f7a4a4c3554e0
whereube <100147393+whereube@users.noreply.github.com>
2024-09-24 10:50:18 +0200

    gjort om cards till list i show all

--- app/client/frontend/src/article/viewArticles/ArticleList.js
+++ app/client/frontend/src/article/viewArticles/ArticleList.js
@@ -1,5 +1,5 @@
 import CategoryFilter from './CategoryFilter';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Modal from './Modal';
 
 const ArticleList = (props) => {
@@ -8,9 +8,9 @@ const ArticleList = (props) => {
     const [currentArticleId, setCurrentArticleId] = useState(null);
 
     const handleInterestClick = (article_id) => {
-        setModalIsOpen(true)
+        setModalIsOpen(true);
         setCurrentArticleId(article_id);
-    }
+    };
 
     const handleAccept = () => {
         props.markAsInterested(currentArticleId);
@@ -19,45 +19,70 @@ const ArticleList = (props) => {
 
     const handleDecline = () => {
         setCurrentArticleId(null);
-        setModalIsOpen(false); 
+        setModalIsOpen(false);
     };
-
 
     return (
         <>
             {props.viewOffers ? (
                 <h1>Tillgängliga erbjudanden</h1>
-            ):(
+            ) : (
                 <h1>Behov</h1>
             )}
-            <CategoryFilter activeCategoryFilter={props.activeCategoryFilter} setFilterByCategory={props.setFilterByCategory}/>
-            <div className='allOffersDiv'>
-                {props.filteredArticles.map(article => (
-                    <div key={article.id} className='offerBox'>
-                        <div className='articleText'>
-                            <p className='offerTitle'>{article.title}</p>
-                            <p className='offerDesc'>{article.description.substr(0, 200)}{article.description.length > 200 ? '...' : ''}</p>
-                        </div>
-                        <div className='articleButtons'>
-                            {props.ownArticleInterest.hasOwnProperty(article.id) ? (
-                                <button className={`button-small offerButton ${props.ownArticleInterest.hasOwnProperty(article.id) ? 'liked': ''}`} onClick={() => props.removeMarkAsInterested(props.ownArticleInterest[article.id].articleInterestId)}>Intresserad {props.articleInterestCounter.hasOwnProperty(article.id) ? props.articleInterestCounter[article.id].count : 0} &#128100;</button>
-                            ):(
-                                <button className={`button-small offerButton`} onClick={() => handleInterestClick(article.id)}> Intresserad {props.articleInterestCounter.hasOwnProperty(article.id) ? props.articleInterestCounter[article.id].count : 0} &#128100;</button>
-                            )}
-                            <button className='button-small offerButton' onClick={() => props.navigateToArticle(article.id)}>Läs mer</button>
-                        </div>
-                        <Modal 
-                            content={<p>Markerar du dig som intresserad på en artikel delas dina mailadress med skaparen av artikeln, vill du detta?</p>}
-                            modalisOpen={modalisOpen}
-                            handleDecline={handleDecline}
-                            handleAccept={handleAccept}
-                            setModalIsOpen={setModalIsOpen}
-                        />
-                    </div>
-                ))}
-            </div>
-        </>
-    )
-}
+            <CategoryFilter activeCategoryFilter={props.activeCategoryFilter} setFilterByCategory={props.setFilterByCategory} />
 
-export default ArticleList
+            <table className='articleTable'>
+                <thead>
+                    <tr>
+                        <th>Titel</th>
+                        <th>Beskrivning</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.filteredArticles.map((article) => (
+                        <tr key={article.id}>
+                            <td className='title-td'>{article.title}</td>
+                            <td>{article.description.substr(0, 60)}{article.description.length > 60 ? '...' : ''}</td>
+                            <td>
+                                <div className='articleButtons'>
+                                    <button
+                                        className='button-small offerButton'
+                                        onClick={() => props.navigateToArticle(article.id)}
+                                    >
+                                        Läs mer
+                                    </button>
+                                    {props.ownArticleInterest.hasOwnProperty(article.id) ? (
+                                        <button
+                                            className={`button-small offerButton ${props.ownArticleInterest.hasOwnProperty(article.id) ? 'liked' : ''}`}
+                                            onClick={() => props.removeMarkAsInterested(props.ownArticleInterest[article.id].articleInterestId)}
+                                        >
+                                            Intresserad {props.articleInterestCounter.hasOwnProperty(article.id) ? props.articleInterestCounter[article.id].count : 0} &#128100;
+                                        </button>
+                                    ) : (
+                                        <button
+                                            className={`button-small offerButton`}
+                                            onClick={() => handleInterestClick(article.id)}
+                                        >
+                                            Intresserad {props.articleInterestCounter.hasOwnProperty(article.id) ? props.articleInterestCounter[article.id].count : 0} &#128100;
+                                        </button>
+                                    )}
+                                    
+                                </div>
+                                <Modal
+                                    content={<p>Markerar du dig som intresserad på en artikel delas dina mailadress med skaparen av artikeln, vill du detta?</p>}
+                                    modalisOpen={modalisOpen}
+                                    handleDecline={handleDecline}
+                                    handleAccept={handleAccept}
+                                    setModalIsOpen={setModalIsOpen}
+                                />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default ArticleList;
