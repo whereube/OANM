52d64a1c1b86057f5f39a4f0918c60b05660b853
Esaiasson <teo.esaiasson@gmail.com>
2026-02-05 18:25:48 +0100

    browser fix

--- app/client/frontend/src/index.js
+++ app/client/frontend/src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+if (typeof requestIdleCallback === 'undefined') {
+  window.requestIdleCallback = function (cb) {
+    return setTimeout(() => {
+      const start = Date.now();
+      cb({
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        },
+      });
+    }, 1);
+  };
+
+  window.cancelIdleCallback = function (id) {
+    clearTimeout(id);
+  };
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
